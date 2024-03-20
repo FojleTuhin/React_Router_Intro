4b582badf776dc1b910431e0d5e1135efaa5a12f
fojletuhin <fojletuhin@gmail.com>
2024-03-20 20:29:50 +0600

    add sweetalert

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,14 +1,26 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import Swal from "sweetalert2";
 
 
 const Home = () => {
+     const clickMe=()=>{
+        Swal.fire({
+            title: "The Internet?",
+            text: "That thing is still around?",
+            icon: "question"
+          });
+     }
+
     return (
         <div>
             <Header></Header>
             
             <Outlet></Outlet>
+
+
+            <button onClick={clickMe}>Click me</button>
 
             <Footer></Footer>
         </div>
